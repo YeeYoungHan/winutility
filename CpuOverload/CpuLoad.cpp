566df77bcd8fd312712fec418826cb22cc9b0fd4
Yee Young Han <websearch@naver.com>
2019-11-18 12:27:12 +0900

    thread 개수 설정 및 loop count 설정 기능을 추가함

--- CpuOverload/CpuLoad.cpp
+++ CpuOverload/CpuLoad.cpp
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
+int giLoopCount = 1;
 
 void PrintString()
 {
@@ -13,9 +16,56 @@ void PrintString()
 	}
 }
 
+DWORD WINAPI LoopThread( LPVOID lpParameter )
+{
+	for( int i = 0; i < giLoopCount; ++i )
+	{
+		PrintString();
+	}
+
+	return 0;
+}
+
+bool StartThread( const char * pszName, LPTHREAD_START_ROUTINE lpStartAddress, LPVOID lpParameter )
+{
+	DWORD		dwThreadId;
+	HANDLE	hThread;
+
+	hThread = CreateThread( NULL, 0, lpStartAddress, lpParameter, 0, &dwThreadId );
+	if( hThread == NULL )
+	{
+		return false;
+	}
+
+	CloseHandle( hThread );
+	
+	return true;
+}
+
 int main( int argc, char * argv[] )
 {
-	PrintString( );
+	if( argc != 3 )
+	{
+		printf( "[Usage] %s {thread count} {loop count}\n", argv[0] );
+		return 0;
+	}
+
+	int iThreadCount = atoi( argv[1] );
+	giLoopCount = atoi( argv[2] );
+
+	for( int i = 0; i < iThreadCount; ++i )
+	{
+		StartThread( "LoopThread", LoopThread, NULL );
+	}
+
+	char c;
+
+	while( 1 )
+	{
+		scanf( "%c", &c );
+
+		if( c == 'x' || c == 'q' ) break;
+	}
 
 	return 0;
 }
