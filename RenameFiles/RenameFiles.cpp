64a98b3ef47b1eed168fe6b8d0d62ead85eb4776
Yee Young Han <websearch@naver.com>
2021-06-17 11:02:06 +0900

    폴더의 파일들을 1 부터 시작되는 숫자로 파일 이름으로 수정하는 기능을 추가함

--- RenameFiles/RenameFiles.cpp
+++ RenameFiles/RenameFiles.cpp
@@ -16,6 +16,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
  */
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <windows.h>
 #include <string>
 
@@ -32,22 +34,60 @@ void ReplaceString( std::string & strCallId, const char * pszBefore, const char 
 	}
 }
 
+bool GetFileExt( const char * pszFilePath, std::string & strExt )
+{
+	strExt.clear();
+
+	if( pszFilePath == NULL ) return false;
+
+	int iLen = (int)strlen( pszFilePath );
+	if( iLen < 2 ) return false;
+
+	if( pszFilePath[iLen-1] == '\\' ) return false;
+
+	for( int i = iLen - 2; i >= 0; --i )
+	{
+		if( pszFilePath[i] == '\\' )
+		{
+			break;
+		}
+
+		if( pszFilePath[i] == '.' )
+		{
+			strExt.append( pszFilePath + i + 1 );
+			return true;
+		}
+	}
+
+	return false;
+}
+
 int main( int argc, char * argv[] )
 {
 	if( argc != 4 )
 	{
 		printf( "[Usage] %s {folder path} {old name} {new name}\n", argv[0] );
+		printf( "[Usage] %s {folder path} * {n}\n", argv[0] );
 		return 0;
 	}
 
 	const char * pszFolderPath = argv[1];
 	const char * pszOldName = argv[2];
 	const char * pszNewName = argv[3];
+	bool bChangeNum = false;
+
+	if( !strcmp( pszOldName, "*" ) & !strcmp( pszNewName, "{n}" ) )
+	{
+		bChangeNum = true;
+	}
 
 	WIN32_FIND_DATA	sttFindData;
 	HANDLE			hFind;
 	BOOL				bNext = TRUE;
 	std::string	strPath = pszFolderPath;
+	std::string strFileName, strOldFileName, strNewFileName, strExt;
+	char				szFileName[255];
+	int					iIndex = 0;
 
 	strPath.append( "\\*.*" );
 
@@ -63,16 +103,34 @@ int main( int argc, char * argv[] )
 		if( !strcmp( sttFindData.cFileName, "." ) || !strcmp( sttFindData.cFileName, ".." ) ) continue;
 		if( sttFindData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) continue;
 
-		if( strstr( sttFindData.cFileName, pszOldName ) )
+		if( bChangeNum )
 		{
-			std::string strFileName = sttFindData.cFileName;
+			if( GetFileExt( sttFindData.cFileName, strExt ) )
+			{
+				++iIndex;
+				_snprintf( szFileName, sizeof(szFileName), "%d.%s", iIndex, strExt.c_str() );
+
+				strOldFileName = pszFolderPath;
+				strOldFileName.append( "\\" );
+				strOldFileName.append( sttFindData.cFileName );
+
+				strNewFileName = pszFolderPath;
+				strNewFileName.append( "\\" );
+				strNewFileName.append( szFileName );
+
+				MoveFile( strOldFileName.c_str(), strNewFileName.c_str() );
+			}
+		}
+		else if( strstr( sttFindData.cFileName, pszOldName ) )
+		{
+			strFileName = sttFindData.cFileName;
 			ReplaceString( strFileName, pszOldName, pszNewName );
 
-			std::string strOldFileName = pszFolderPath;
+			strOldFileName = pszFolderPath;
 			strOldFileName.append( "\\" );
 			strOldFileName.append( sttFindData.cFileName );
 
-			std::string strNewFileName = pszFolderPath;
+			strNewFileName = pszFolderPath;
 			strNewFileName.append( "\\" );
 			strNewFileName.append( strFileName );
 
